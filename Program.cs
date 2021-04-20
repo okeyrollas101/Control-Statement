be321fe27e2937f5382985dd9dc0b5eae873d639
OKECHUKWU <obilekekachi@gmail.com>
2021-04-20 16:23:34 +0100

    second commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Control_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // it involves if statement, if....else statement, else if and switch statment;
+          int num = 20;
+          if (num < 20)
+          {
+              
+          }
+        }
+    }
+}
